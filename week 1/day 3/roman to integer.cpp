91b5f05f54153c551be154cf99f4dc5a5e38468f
Shambhavi Gupta <guptashambhavi022@gmail.com>
2026-06-03 08:58:03 +0530

    Implement Roman to Integer conversion function

---
+++ week 1/day 3/roman to integer.cpp
@@ -0,0 +1,22 @@
+lass Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> m = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+
+        int total = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            // If next value is bigger → subtract
+            if (i + 1 < s.size() && m[s[i]] < m[s[i + 1]]) {
+                total -= m[s[i]];
+            } else {
+                total += m[s[i]];
+            }
+        }
+
+        return total;
+    }
+};
