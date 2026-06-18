a97ac4feaca0bea2ed639476d3ee22d2e8a1cf4d
Shambhavi Gupta <guptashambhavi022@gmail.com>
2026-06-18 13:02:43 +0530

    Create largest rectangle in histogram.cpp

---
+++ week 3/day_4/largest rectangle in histogram.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+
+        stack<int> st;
+        int ans = 0;
+
+        heights.push_back(0);
+
+        for(int i = 0; i < heights.size(); i++) {
+
+            while(!st.empty() && heights[st.top()] > heights[i]) {
+
+                int h = heights[st.top()];
+                st.pop();
+
+                int w;
+
+                if(st.empty())
+                    w = i;
+                else
+                    w = i - st.top() - 1;
+
+                ans = max(ans, h * w);
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
