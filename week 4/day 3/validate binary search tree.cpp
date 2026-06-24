da7ca179b80f0dab5409cce78b2d033136ed5fb5
Shambhavi Gupta <guptashambhavi022@gmail.com>
2026-06-24 15:28:09 +0530

    Create validate binary search tree.cpp

---
+++ week 4/day 3/validate binary search tree.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    bool check(TreeNode* root, long long low, long long high) {
+
+        if(root == NULL)
+            return true;
+
+        if(root->val <= low || root->val >= high)
+            return false;
+
+        return check(root->left, low, root->val) &&
+               check(root->right, root->val, high);
+    }
+
+    bool isValidBST(TreeNode* root) {
+
+        return check(root, LLONG_MIN, LLONG_MAX);
+    }
+};
